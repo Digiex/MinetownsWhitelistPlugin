f98bb607a3d17933ea49848a84b1d1ecf8239044
jessenic <jessenic@digiex.net>
2013-10-23 15:43:29 +0300

    Started working on the simplified version

--- src/main/java/net/minetowns/whitelist/WLPlugin.java
+++ src/main/java/net/minetowns/whitelist/WLPlugin.java
@@ -1,11 +1,9 @@
 package net.minetowns.whitelist;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import net.milkbowl.vault.permission.Permission;
 
-import org.apache.commons.lang.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -19,47 +17,51 @@ import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class WLPlugin extends JavaPlugin implements Listener {
-	
+
 	private Permission provider;
 
 	@EventHandler
 	public void OnPlayerJoin(PlayerJoinEvent e) {
-		if (e.getPlayer().isOp()
-				|| e.getPlayer().hasPermission("wlplugin.admin")) {
-			String message = listWPlayers(false);
-			if (message.length() > 1) {
-				e.getPlayer().sendMessage(message);
-			}
+		if (needsWhitelist(e.getPlayer())) {
+			e.getPlayer()
+					.sendMessage(
+							ChatColor.AQUA
+									+ ""
+									+ ChatColor.UNDERLINE
+									+ "Notice:"
+									+ ChatColor.RED
+									+ " You need to accept the rules before you can build. "
+									+ ChatColor.GREEN
+									+ "Type /rules to continue.");
 		}
-	}
-
-	public String listWPlayers(Boolean retNoPlayers) {
-		List<String> reqList = this.getConfig().getStringList("requests");
-		if (reqList != null && reqList.size() > 0) {
-			return ChatColor.BOLD
-					+ ""
-					+ ChatColor.BLUE
-					+ "[Whitelist] The following players have requested whitelisting: "
-					+ ChatColor.RED + "" + ChatColor.UNDERLINE
-					+ StringUtils.join(reqList, ", ") + ChatColor.RESET
-					+ " Whitelist using /wl add <player>";
-		} else if (retNoPlayers) {
-			return ChatColor.BLUE + "[Whitelist] No players in whitelist queue";
-		}
-		return "";
 	}
 
 	@Override
 	public void onEnable() {
-            if (getVault() == null) {
-                getServer().getPluginManager().disablePlugin(this);
-            }
+		if (getVault() == null) {
+			getServer().getPluginManager().disablePlugin(this);
+		}
+		if (getConfig().getStringList("rules") == null
+				|| getConfig().getStringList("rules").size() == 0) {
+			ArrayList<String> ruleList = new ArrayList<String>();
+			ruleList.add(ChatColor.RED + "Rules:");
+			ruleList.add(ChatColor.BLUE + "Use common sense!");
+			getConfig().set("rules", ruleList);
+			saveConfig();
+		}
 		getServer().getPluginManager().registerEvents(this, this);
 		getServer().getScheduler().runTaskTimer(this, new Runnable() {
+			@Override
 			public void run() {
 				for (Player player : getServer().getOnlinePlayers()) {
 					if (needsWhitelist(player)) {
-						player.sendMessage("Notice: Your not currently whitelisted and unable to build. Sign up and automatically whitelist @ Minetowns.net");
+						player.sendMessage(ChatColor.AQUA
+								+ ""
+								+ ChatColor.UNDERLINE
+								+ "Notice:"
+								+ ChatColor.RED
+								+ " You need to accept the rules before you can build. "
+								+ ChatColor.GREEN + "Type /rules to continue.");
 					}
 				}
 			}
@@ -69,149 +71,34 @@ public class WLPlugin extends JavaPlugin implements Listener {
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label,
 			String[] args) {
-		if (cmd.getName().equalsIgnoreCase("wl")) {
-			if (args.length < 1) {
-				return false;
-			}
-			if (!sender.isOp()) {
-				sender.sendMessage(ChatColor.RED
-						+ "[Whitelist] You don't have permission!");
-				return true;
-			}
-			List<String> reqList = this.getConfig().getStringList("requests");
-			if (reqList == null) {
-				reqList = new ArrayList<String>();
-			}
-			if (args[0].equalsIgnoreCase("add")) {
-				if (args.length < 2) {
-					return false;
-				}
-				String pname = args[1];
-				List<String> nameMatch = matchOfflinePlayer(pname);
-				if (nameMatch.size() > 1) {
-					sender.sendMessage(ChatColor.RED
-							+ "[Whitelist] Multiple players found. ("
-							+ StringUtils.join(nameMatch, ", ")
-							+ ") Please be more specific.");
+		if (cmd.getName().equalsIgnoreCase("rules")) {
+			if (args.length > 0) {
+				if (args[0].equalsIgnoreCase("reload") && sender.isOp()) {
+					this.reloadConfig();
+					sender.sendMessage("Rules reloaded");
 					return true;
-				} else if (nameMatch.size() == 1) {
-					pname = nameMatch.get(0);
-				}
-				if (reqList.contains(pname)) {
-					this.getServer().dispatchCommand(sender, "lookup " + pname);
-					if (this.getServer().dispatchCommand(sender,
-							"pex user " + pname + " group set Member")) {
-						reqList.remove(pname);
-						this.getConfig().set("requests", reqList);
-						this.saveConfig();
-						this.getLogger().info(
-								sender.getName() + " whitelisted " + pname);
-						sender.sendMessage(ChatColor.BLUE + "[Whitelist] "
-								+ pname + " whitelisted!");
-						for (Player p : this.getServer().getOnlinePlayers()) {
-							if (p.getName().equalsIgnoreCase(pname)) {
-								p.sendMessage(ChatColor.BLUE
-										+ "[Whitelist] You are now whitelisted. Enjoy your stay");
-								break;
-							}
-						}
-					} else {
-						sender.sendMessage(ChatColor.RED
-								+ "[Whitelist] Pex set group failed");
-					}
-				} else {
-					sender.sendMessage(ChatColor.RED + "[Whitelist] Player "
-							+ pname + " not in the whitelisting queue. See /"
-							+ cmd.getName() + " list");
-				}
-			} else if (args[0].equalsIgnoreCase("list")) {
-				sender.sendMessage(listWPlayers(true));
-			} else if (args[0].equalsIgnoreCase("addtolist")) {
-				if (args.length < 2) {
-					return false;
-				}
-				Player player = getServer().getPlayer(args[1]);
-				if (player == null) {
-					return false;
-				}
-				if (!needsWhitelist(player)) {
-					sender.sendMessage(player.getName() + " does not need to be whitelisted.");
+				} else if (args[0].equalsIgnoreCase("accept")) {
+					// TODO: accept
+					sender.sendMessage("Not implemented yet");
 					return true;
-				}
-				if (!reqList.contains(args[1])) {
-					reqList.add(args[1]);
-					this.getConfig().set("requests", reqList);
-					this.saveConfig();
-					sender.sendMessage("[Whitelist] " + args[1]
-							+ " added into the whitelist queue.");
-					this.getLogger().info(
-							args[1] + " added into the whitelist queue.");
-					for (Player p : this.getServer().getOnlinePlayers()) {
-						if (p.isOp() || p.hasPermission("wlplugin.admin")) {
-							p.sendMessage(listWPlayers(false));
-						}
-						if (p.getName().equalsIgnoreCase(args[1])) {
-							p.sendMessage(ChatColor.BLUE
-									+ "[Whitelist] You are now added to the whitelist waiting queue. Please contact an admin to get whitelisted.");
-						}
-					}
-				} else {
-					sender.sendMessage("[Whitelist] " + args[1]
-							+ " was already in the whitelist queue!");
-				}
-			} else if (args[0].equalsIgnoreCase("check")) {
-				if (args.length < 2) {
-					return false;
-				}
-				try {
-					if (needsWhitelist(getServer().getPlayer(args[1]))) {
-						sender.sendMessage("true");
-					} else {
-						sender.sendMessage("false");
-					}
-				} catch (Exception ex) {
-					sender.sendMessage("false");
-				}
-			} else if (args[0].equalsIgnoreCase("remove")) {
-				if (args.length < 2) {
-					return false;
-				}
-				String pname = args[1];
-				List<String> nameMatch = matchOfflinePlayer(pname);
-				if (nameMatch.size() > 1) {
-					sender.sendMessage(ChatColor.RED
-							+ "[Whitelist] Multiple players found. ("
-							+ StringUtils.join(nameMatch, ", ")
-							+ ") Please be more specific.");
-					return true;
-				} else if (nameMatch.size() == 1) {
-					pname = nameMatch.get(0);
-				}
-				if (reqList.contains(pname)) {
-					reqList.remove(pname);
-					this.getConfig().set("requests", reqList);
-					this.saveConfig();
-					sender.sendMessage(ChatColor.BLUE + "[Whitelist] Player "
-							+ pname + " removed from the whitelist queue.");
-					this.getLogger().info(
-							pname + " removed from the whitelist queue.");
-				} else {
-					sender.sendMessage(ChatColor.RED + "[Whitelist] Player "
-							+ pname + " not in the whitelisting queue. See /"
-							+ cmd.getName() + " list");
 				}
 			} else {
-				return false;
+				for (String rule : this.getConfig().getStringList("rules")) {
+					sender.sendMessage(rule);
+				}
+				sender.sendMessage(ChatColor.GREEN
+						+ "Accept the rules by typing " + ChatColor.UNDERLINE
+						+ "/rules accept");
+				return true;
 			}
-			return true;
 		}
 		return false;
 	}
-	
+
 	private Permission getVault() {
-    	if (provider != null) {
-    		return provider;
-    	}
+		if (provider != null) {
+			return provider;
+		}
 		Plugin plugin = getServer().getPluginManager().getPlugin("Vault");
 		if (plugin == null || !(plugin instanceof net.milkbowl.vault.Vault)) {
 			getServer().getPluginManager().disablePlugin(this);
@@ -226,30 +113,6 @@ public class WLPlugin extends JavaPlugin implements Listener {
 		return provider = rsp.getProvider();
 	}
 
-	public List<String> matchOfflinePlayer(String partialName) {
-		List<String> reqList = this.getConfig().getStringList("requests");
-		List<String> matchedOfflinePlayers = new ArrayList<String>();
-		if (reqList != null) {
-			List<String> found = new ArrayList<String>();
-			for (String player : reqList) {
-				if (!found.contains(player)) {
-					found.add(player);
-					if (partialName.equalsIgnoreCase(player)) {
-						// Exact match
-						matchedOfflinePlayers.clear();
-						matchedOfflinePlayers.add(player);
-						break;
-					}
-					if (player.toLowerCase().indexOf(partialName.toLowerCase()) != -1) {
-						// Partial match
-						matchedOfflinePlayers.add(player);
-					}
-				}
-			}
-		}
-		return matchedOfflinePlayers;
-	}
-	
 	public boolean needsWhitelist(Player player) {
 		return getVault().getPrimaryGroup(player).equals("Player");
 	}
